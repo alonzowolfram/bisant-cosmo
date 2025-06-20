2cdd8b271920d0bce64fd5b2d5747397cf3edb56
Lon Fong <alonzo.fong@gmail.com>
2025-06-20 15:14:30 -0500

    Removed deprecated code to make code maintenance easier

--- src/pipeline/data_import_cleaning.R
+++ src/pipeline/data_import_cleaning.R
@@ -140,54 +140,6 @@ for(i in 1:length(slide_names)) {
   
   close(pb)
   
-  # last_chunk <- FALSE 
-  # skiprows <- 0
-  # chunk_id <- 1
-  # 
-  # required_cols <- data.table::fread(counts_file, select=c("fov", "cell_ID"))
-  # stopifnot("columns 'fov' and 'cell_ID' are required, but not found in the counts file" = 
-  #             all(c("cell_ID", "fov") %in% colnames(required_cols)))
-  # number_of_cells <- nrow(required_cols)
-  # 
-  # number_of_cols <-  ncol(data.table::fread(counts_file, nrows = 2))
-  # number_of_chunks <- ceiling(as.numeric(number_of_cols) * as.numeric(number_of_cells) / (nonzero_elements_per_chunk)) # Because of INTEGER OVERFLOW, must convert integers to numeric to avoid running out of space when doing integer multiplication (R only does 32-bit for integers I guess.)
-  # chunk_size <- floor(number_of_cells / number_of_chunks)
-  # sub_counts_matrix <- vector(mode='list', length=number_of_chunks)
-  # 
-  # pb <- txtProgressBar(min = 0, max = number_of_chunks, initial = 0, char = "=",
-  #                      width = NA, title, label, style = 3, file = "")
-  # cell_count <- 0
-  # system.time({
-  #   while(last_chunk==FALSE) {
-  #     read_header <- FALSE
-  #     if(chunk_id==1){
-  #       read_header <- TRUE
-  #     }
-  #     
-  #     counts_data_table <- data.table::fread(counts_file,
-  #                                            nrows = chunk_size,
-  #                                            skip = skiprows + (chunk_id > 1),
-  #                                            header = read_header)
-  #     if(chunk_id == 1){
-  #       header <- colnames(counts_data_table)
-  #     } else {
-  #       colnames(counts_data_table) <- header
-  #     }
-  #     
-  #     cell_count <- nrow(counts_data_table) + cell_count     
-  #     if(cell_count == number_of_cells) last_chunk <- TRUE
-  #     skiprows <- skiprows + chunk_size
-  #     slide_fov_cell_counts <- paste0("c_", i, "_", slide_ID_numeric, "_", counts_data_table$fov, "_", counts_data_table$cell_ID) # As of 2024/10/18, we are adding i to the identifier, which is the slide # in a given data analysis. slide_ID_numeric is the slide # in a given CosMx _run_ (physical). This will allow us to merge data from slides that come from different runs and may therefore have the same slide_ID_numeric. We will have to do the same with slide_fov_cell_metadata below. 
-  #     sub_counts_matrix[[chunk_id]] <- as(counts_data_table[,-c("fov", "cell_ID"),with=FALSE], "sparseMatrix") 
-  #     rownames(sub_counts_matrix[[chunk_id]]) <- slide_fov_cell_counts 
-  #     setTxtProgressBar(pb, chunk_id)
-  #     chunk_id <- chunk_id + 1
-  #   }
-  # })
-  # # user  system elapsed 
-  # # 6.055   0.625   6.701 
-  # close(pb)   
-  
   count_list[[i]] <- do.call(rbind, sub_counts_matrix) 
   
   # 2024/10/18: add a column for run ID.
